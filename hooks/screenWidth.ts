a14e1401e18d48f8e3c8bfe995f726e7d120d334
Chad Rossouw <chadrossouw7247@gmail.com>
2023-09-19 00:31:18 -0400

    fix(frontend): window not defined in custom hook

--- hooks/screenWidth.ts
+++ hooks/screenWidth.ts
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 const useCheckMobileScreen = () => {
-    const [width, setWidth] = useState(window.innerWidth);
+    const [width, setWidth] = useState(0);
     const handleWindowSizeChange = () => {
         setWidth(window.innerWidth);
     }
 
     useEffect(() => {
+        window.addEventListener('DOMContentLoaded', handleWindowSizeChange);
         window.addEventListener('resize', handleWindowSizeChange);
         return () => {
+            window.removeEventListener('DOMContentLoaded', handleWindowSizeChange);
             window.removeEventListener('resize', handleWindowSizeChange);
         }
     }, []);

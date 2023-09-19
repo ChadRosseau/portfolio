f0cec4f0562f6121ca3185c16bbcb2c6d6506fbf
Chad Rossouw <chadrossouw7247@gmail.com>
2023-09-18 23:59:38 -0400

    feat(skills): mobile

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,9 +1,13 @@
-import Head from "next/head";
+"use client";
+
 import SkillGroup from "./skillGroup";
 import styles from "./skills.module.scss";
 import { Skills } from "@/data/skill-lib";
+import useCheckMobileScreen from "@/hooks/screenWidth";
 
 const SkillsPage = () => {
+    const isMobile = useCheckMobileScreen();
+
     return (
         <div className={styles.skillsWrapper}>
             <div className={styles.columnWrapper}>
@@ -16,46 +20,46 @@ const SkillsPage = () => {
                     <SkillGroup
                         groupName="Frameworks"
                         group={Skills.Frameworks}
-                        delay={2.25}
+                        delay={isMobile ? 0.5 : 2.25}
                     />
                     <SkillGroup
                         groupName="Mobile"
                         group={Skills.Mobile}
-                        delay={3.75}
+                        delay={isMobile ? 0.75 : 3.75}
                     />
                 </div>
                 <div className={styles.skillsColumn}>
                     <SkillGroup
                         groupName="Backend"
                         group={Skills.Backend}
-                        delay={0.5}
+                        delay={isMobile ? 1 : 0.5}
                     />
                     <SkillGroup
                         groupName="Database"
                         group={Skills.Database}
-                        delay={1.25}
+                        delay={isMobile ? 1.25 : 1.25}
                     />
                     <SkillGroup
                         groupName="Technologies"
                         group={Skills.Technologies}
-                        delay={2.75}
+                        delay={isMobile ? 1.75 : 2.75}
                     />
                 </div>
                 <div className={styles.skillsColumn}>
                     <SkillGroup
                         groupName="Cloud"
                         group={Skills.Cloud}
-                        delay={0.75}
+                        delay={isMobile ? 2 : 0.75}
                     />
                     <SkillGroup
                         groupName="Other Languages"
                         group={Skills.Languages}
-                        delay={1.75}
+                        delay={isMobile ? 2.25 : 1.75}
                     />
                     <SkillGroup
                         groupName="APIs"
                         group={Skills.APIs}
-                        delay={3.25}
+                        delay={isMobile ? 2.5 : 3.25}
                     />
                 </div>
             </div>

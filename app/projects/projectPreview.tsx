b5e795d928767efe33c8e866d04d18ed7da49259
Chad Rossouw <chadrossouw7247@gmail.com>
2025-12-20 07:10:12 +0800

    fix: css property

--- app/projects/projectPreview.tsx
+++ app/projects/projectPreview.tsx
@@ -6,45 +6,50 @@ import Link from "next/link";
 import { CSSProperties } from "react";
 
 type ProjectPreviewProps = {
-  id: string;
-  project: Project;
-  delay: string;
-  isMobile: boolean;
+	id: string;
+	project: Project;
+	delay: string;
+	isMobile: boolean;
 };
 
-const ProjectPreview = ({ id, project, delay, isMobile }: ProjectPreviewProps) => {
-  const style: CSSProperties = {
-    ["animation-delay" as any]: delay,
-    ["background-image" as any]: `url('${project.photo_url}`,
-  };
+const ProjectPreview = ({
+	id,
+	project,
+	delay,
+	isMobile,
+}: ProjectPreviewProps) => {
+	const style: CSSProperties = {
+		["animationDelay" as any]: delay,
+		["backgroundImage" as any]: `url('${project.photo_url}`,
+	};
 
-  return (
-    <Link href={`project/${id}`}>
-      <div className={styles.projectPreview} style={style}>
-        <div className={styles.bar}>
-          <div>
-            <h4>{project.name}</h4>
-            <div className={styles.projectTags}>
-              {project.skills.map((skill: SkillTag, i: number) => {
-                return (isMobile ? i < 3 : i < 5) ? (
-                  <Image
-                    src={`skills_icons/color/${skill}.svg`}
-                    alt={skill + " logo"}
-                    key={i}
-                    width={30}
-                    height={30}
-                  />
-                ) : (
-                  ""
-                );
-              })}
-            </div>
-          </div>
-          <p>{project.synopsis}</p>
-        </div>
-      </div> </Link>
-
-  );
+	return (
+		<Link href={`project/${id}`}>
+			<div className={styles.projectPreview} style={style}>
+				<div className={styles.bar}>
+					<div>
+						<h4>{project.name}</h4>
+						<div className={styles.projectTags}>
+							{project.skills.map((skill: SkillTag, i: number) => {
+								return (isMobile ? i < 3 : i < 5) ? (
+									<Image
+										src={`skills_icons/color/${skill}.svg`}
+										alt={skill + " logo"}
+										key={i}
+										width={30}
+										height={30}
+									/>
+								) : (
+									""
+								);
+							})}
+						</div>
+					</div>
+					<p>{project.synopsis}</p>
+				</div>
+			</div>{" "}
+		</Link>
+	);
 };
 
 export default ProjectPreview;
